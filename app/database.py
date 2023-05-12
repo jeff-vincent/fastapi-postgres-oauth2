dcf897a90857707d4921a7aba86dba83b8c98323
jeff-vincent <jeff.d.vincent@gmail.com>
2023-05-12 11:30:42 -0700

    Add docker-compose

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost:5432/example_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@postgres:5432/example_db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
